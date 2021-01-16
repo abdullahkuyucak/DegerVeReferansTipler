9976b50e7c7a095fb7983cf772d7cacecafcfbdd
Abdullah Kuyucak <42abdullah.kuyucak09@gmail.com>
2021-01-17 00:29:24 +0300

    Proje dosyası ekle.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int,decimal,float,double,bool=değer tip
+            //array=referans tip
+            Console.WriteLine("Hello World!");
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            Console.WriteLine(sayi1);
+        
+        }
+
+    }
+}
